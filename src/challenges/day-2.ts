2334df3f1d1bcf2346131fcabcc86495ad9adb92
Abderrahmane Tahri Jouti <tj.abderrahmane@gmail.com>
2021-12-03 14:13:06 +0100

    init day 2

---
+++ src/challenges/day-2.ts
@@ -0,0 +1,5 @@
+import input from "./input/day-2"
+
+export const day2 = () => {
+  console.log(input)
+}
--- src/programs.ts
+++ src/programs.ts
@@ -1,5 +1,6 @@
 import { day1 } from "./challenges/day-1"
 import { day1_2 } from "./challenges/day-1_2"
+import { day2 } from "./challenges/day-2"
 
 export const programs = new Map<string, () => void>()
 
@@ -13,3 +14,4 @@ export const defineProgram = (name: string, program: () => void) => {
 
 defineProgram("day 1", day1)
 defineProgram("day 1_2", day1_2)
+defineProgram("day 2", day2)
