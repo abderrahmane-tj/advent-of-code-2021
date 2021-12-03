24f31ec6f1c9022092dc662542946c00a15e7207
Abderrahmane Tahri Jouti <tj.abderrahmane@gmail.com>
2021-12-03 15:58:51 +0100

    fix day 2

--- src/challenges/day-2.ts
+++ src/challenges/day-2.ts
@@ -1,5 +1,24 @@
 import input from "./input/day-2"
 
 export const day2 = () => {
-  console.log(input)
+  const instructions = input.split("\n")
+  let Z = 0
+  let Y = 0
+  instructions.forEach((instruction) => {
+    const [direction, distance] = instruction.split(" ")
+    switch (direction) {
+      case "forward":
+        Z += Number(distance)
+        break
+      case "up":
+        Y -= Number(distance)
+        break
+      case "down":
+        Y += Number(distance)
+        break
+      default:
+        throw new Error("Shouldn't be here")
+    }
+  })
+  console.log(Z * Y)
 }
