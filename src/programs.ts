e06f868627550c5bc09b02c1da69bb4f11955ea3
Abderrahmane Tahri Jouti <tj.abderrahmane@gmail.com>
2021-12-03 16:11:54 +0100

    add day 2 part 2

--- src/programs.ts
+++ src/programs.ts
@@ -1,6 +1,7 @@
 import { day1 } from "./challenges/day-1"
 import { day1_2 } from "./challenges/day-1_2"
 import { day2 } from "./challenges/day-2"
+import { day2_2 } from "./challenges/day-2_2"
 
 export const programs = new Map<string, () => void>()
 
@@ -15,3 +16,4 @@ export const defineProgram = (name: string, program: () => void) => {
 defineProgram("day 1", day1)
 defineProgram("day 1_2", day1_2)
 defineProgram("day 2", day2)
+defineProgram("day 2_2", day2_2)
